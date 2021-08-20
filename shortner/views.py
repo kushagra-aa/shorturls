71dd7a0a90daf421dc1f31b0b5543934d4f93f20
kushagra-aa <kushagraagnihotri24@gmail.com>
2021-08-20 21:02:09 +0530

    initial commit

---
+++ shortner/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+import uuid
+from .models import Urls
+from django.http import HttpResponse
+# Create your views here.
+
+
+def go(request, pk):
+    url_details = Urls.objects.get(uuid=pk)
+    return redirect(url_details.link)
+
+
+def create(request):
+    if request.method == 'POST':
+        url = request.POST['link']
+        uid = str(uuid.uuid4())[:5]
+        new_url = Urls(link=url, uuid=uid)
+        new_url.save()
+        return HttpResponse(uid)
+
+
+def index(request):
+    return render(request, "index.html")
---
+++ static/js/script.js
@@ -0,0 +1,16 @@
+site_url = "shoten.gg"
+local_url = "http://127.0.0.1:8000"
+$(document).on("submit", "#post-form", function (e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "/create",
+        data: {
+            link: $("#link").val(),
+            csrfmiddlewaretoken: $("input[name=csrfmiddlewaretoken]").val(),
+        },
+        success: function (data) {
+            $("h2").html(local_url + "/" + data);
+        },
+    });
+});
